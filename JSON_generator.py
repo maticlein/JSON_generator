f27c7a14d544284045496e0ffc0da9575585488d
Matthias Clein <m.cleinespinoza@gmail.com>
2020-11-12 02:22:00 -0300

    Se añade capacidad de escribir más de una línea por campo en la plantilla

--- JSON_generator.py
+++ JSON_generator.py
@@ -6,6 +6,21 @@ myList = [item for item in text.split('\n')]
 asignatura = myList[2] + "-" + myList[6]
 print("Procesando: " + asignatura)
 
+sections = ["Fecha de inicio",
+            "Fecha de termino",
+            "Modalidad",
+            "Nivel",
+            "Horas",
+            "Descripcion",
+            "Proposito",
+            "Objetivos",
+            "Unidades",
+            "Calendario de actividades",
+            "Docentes",
+            "Evaluaciones",
+            "Resultados de aprendizaje",
+            "Link de Classroom"]
+
 titles = ["inicio",
           "termino",
           "modalidad",
@@ -38,11 +53,29 @@ messages = ["La fecha de inicio de la asignatura " + asignatura + " es el ",
 
 position = 0
 
-for i in range(8,len(myList),4):
-  print(myList[i])
+print("Generando archivos JSON....")
+
+j = 8
+
+for i in range(14):
+  print(myList[j] + " -> OK")
+  content = ""
+  if myList[j] != sections[-1]:
+    j += 1
+    while myList[j] != sections[position + 1]:
+      if myList[j] != "":
+        content += myList[j] + "\n"
+      j += 1
+  else:
+    j += 1
+    while j < len(myList):
+      if myList[j] != "":
+        content += myList[j] + "\n"
+      j += 1
+  content = content.rstrip("\n")
   x = {
     "title": asignatura,
-    "text": messages[position] + myList[i+2] 
+    "text": messages[position] + content 
   }
   y = json.dumps(x)
   filename = myList[2] + "_" + titles[position] + ".json"
